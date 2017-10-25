032ae653c1659eb46cf0b809df0ab04d971b7c0b
sunny8751 <sunny8751@gmail.com>
2017-10-24 20:16:14 -0400

    Shows sightings at specific times

--- app/src/main/java/com/piedpiper/MainActivity.java
+++ app/src/main/java/com/piedpiper/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,18 +14,27 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private String accountType;
     private DatabaseReference database;
+    public static List<RatSighting> sightingsList;
+//    private DatabaseReference dataRef;
+    public Query snap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sightingsList = new LinkedList<>();
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
@@ -45,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                System.out.println("signed out");
                 if (user == null) {
                     // user auth state is changed - user is null
                     // launch login activity
@@ -89,6 +98,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 accountType = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+//        dataRef = database.child("sightings");
+
+        snap = database.child("sightings").limitToLast(500);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("sunwoo", "started main");
+
+        snap.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("sunwoo", "added to list");
+                sightingsList.clear();
+                for(DataSnapshot sightingSnapshot: dataSnapshot.getChildren()) {
+                    RatSighting add = new RatSighting();
+                    add.setCity((String) sightingSnapshot.child("City").getValue());
+                    add.setBorough((String) sightingSnapshot.child("Borough").getValue());
+                    add.setIncidentAddress((String) sightingSnapshot.child("Incident Address").getValue());
+                    add.setIncidentZip((String) sightingSnapshot.child("Incident Zip").getValue());
+                    add.setCreatedDate((String) sightingSnapshot.child("Created Date").getValue());
+                    add.setLocationType((String) sightingSnapshot.child("Location Type").getValue());
+                    add.setLatitude((String) sightingSnapshot.child("Latitude").getValue());
+                    add.setLongitude((String) sightingSnapshot.child("Longitude").getValue());
+                    add.setUniqueKey(sightingSnapshot.getKey());
+                    sightingsList.add(0, add);
+                }
             }
 
             @Override
--- app/src/main/java/com/piedpiper/SightingsMapActivity.java
+++ app/src/main/java/com/piedpiper/SightingsMapActivity.java
@@ -3,6 +3,8 @@ package com.piedpiper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,11 +13,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import static com.piedpiper.R.id.map;
+
 /**
  * Created by pbokey on 10/9/17.
  */
 
 public class SightingsMapActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    Date start, end;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,17 +36,53 @@ public class SightingsMapActivity extends AppCompatActivity implements OnMapRead
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(map);
         mapFragment.getMapAsync(this);
+
+        // set default start and end
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.YEAR,2017);
+        start = cal.getTime();
+        cal.set(Calendar.MONTH, Calendar.MARCH);
+        cal.set(Calendar.YEAR,2017);
+        end = cal.getTime();
+
+
+        Button setStartButton = (Button) findViewById(R.id.mapstartdate_button_id);
+        Button setEndButton = (Button) findViewById(R.id.mapenddate_button_id);
+
+        setStartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // new DatePickerDialog
+            }
+        });
+        setEndButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // new DatePickerDialog
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        List<RatSighting> sightings = MainActivity.sightingsList;
+        int count = 0;
+        for (RatSighting sighting : sightings) {
+            Date date;
+            try {
+                date = new SimpleDateFormat("MM/dd/yyyy  hh:mm:ss a").parse(sighting.getCreatedDate());
+            } catch (ParseException e) {
+                date = new Date();
+            }
+            //date.compareTo(start) >= 0 && date.compareTo(end) <= 0
+            if (count++ < 10) {
+                // between start and end
+                LatLng location = new LatLng(Double.parseDouble(sighting.getLatitude()), Double.parseDouble(sighting.getLongitude()));
+                googleMap.addMarker(new MarkerOptions().position(location)
+                        .title(sighting.getUniqueKey()));
+            }
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.7128, -74.0060), 11.0f));
     }
 }
