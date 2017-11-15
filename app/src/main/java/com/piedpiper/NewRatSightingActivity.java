cfb20d8bfe80af39955566b85899302e3fdf0e71
Rahul <rahulajmera016@gmail.com>
2017-11-15 13:45:11 -0500

    Test if rat sighting added

--- app/src/main/java/com/piedpiper/NewRatSightingActivity.java
+++ app/src/main/java/com/piedpiper/NewRatSightingActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,10 +48,6 @@ public class NewRatSightingActivity extends AppCompatActivity{
 //        uniqueID.setText(uniqueKey);
 
 
-        /**
-         * Rahul JUnits
-         *
-         */
         Button add = (Button) findViewById(R.id.addratsighting_button_id);
         add.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -58,7 +55,12 @@ public class NewRatSightingActivity extends AppCompatActivity{
                 DatabaseReference dataRef = FirebaseDatabase.getInstance().getReference("sightings");
                 DatabaseReference newRef = dataRef.push();
                 newSighting.setUniqueKey(newRef.getKey());
+                String key = newRef.getKey();
                 newRef.setValue(newSighting.getMap());
+                
+                if (dataRef.child(key) != null) {
+                    Log.d("rahul", dataRef.child(key) + "is here");
+                }
                 Intent splash = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(splash);
             }
