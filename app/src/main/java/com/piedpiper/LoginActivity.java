8e84c3f89b8277f80c839247cd226210c7f0f609
Rahul <rahulajmera016@gmail.com>
2017-11-19 16:55:53 -0500

    Rahul Tests

--- app/src/main/java/com/piedpiper/LoginActivity.java
+++ app/src/main/java/com/piedpiper/LoginActivity.java
@@ -38,51 +38,53 @@ public class LoginActivity extends AppCompatActivity {
             finish();
         }
 
-        Button loginButton = findViewById(R.id.login_button_id);
+        final Button loginButton = findViewById(R.id.login_button_id);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userName = ((EditText) findViewById(R.id.username_text_input)).getText().toString().trim().toLowerCase();
                 String password = ((EditText) findViewById(R.id.password_text_input)).getText().toString();
-                if (TextUtils.isEmpty(userName)) {
+                String loginAction = login(userName, password);
+
+                if (loginAction.equals("No username")) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                if (TextUtils.isEmpty(password)) {
+                if (loginAction.equals("No password")) {
                     Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
                     return;
-                }
+                } else {
+
                 auth.signInWithEmailAndPassword(userName, password)
-                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            // If sign in fails, display a message to the user. If sign in succeeds
-                            // the auth state listener will be notified and logic to handle the
-                            // signed in user can be handled in the listener.
-                            if (!task.isSuccessful()) {
-                                // there was an error
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    // there was an error
 
-                                //make an error message if you register with the same email
+                                    //make an error message if you register with the same email
 
-                                AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
-                                alertDialog.setTitle("Incorrect Login Information");
-                                alertDialog.setMessage("Please provide correct credentials");
-                                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                dialog.dismiss();
-                                            }
-                                        });
-                                alertDialog.show();
-                            } else {
-                                Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                                startActivity(intent);
-                                finish();
+                                    AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+                                    alertDialog.setTitle("Incorrect Login Information");
+                                    alertDialog.setMessage("Please provide correct credentials");
+                                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                            new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    dialog.dismiss();
+                                                }
+                                            });
+                                    alertDialog.show();
+                                } else {
+                                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
                             }
-                        }
-                    });
+                        }); }
             }
         });
 
@@ -94,6 +96,33 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(splash);
             }
         });
+    }
+
+    /**
+     * Method to determine whether or not to try logging in
+     * @param userName login credential
+     * @param password login credential
+     * @return String to detemine what message to show or to login
+     */
+    public String login(String userName, String password) {
+//        if (TextUtils.isEmpty(userName)) {
+//            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+//            return "No username";
+//        }
+//
+//        if (TextUtils.isEmpty(password)) {
+//            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+//            return "No password";
+//        }
+
+        if (userName.equals("")) {
+            return "No username";
+        }
+
+        if (password.equals("")) {
+            return "No password";
+        }
+        return "login";
     }
 
 
