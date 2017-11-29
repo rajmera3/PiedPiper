0f4fff8d4b2445c46441722130f04a263406f7fe
Rahul <rahulajmera016@gmail.com>
2017-11-28 19:49:55 -0500

    Location Services to fill in Longitude and Latitude

--- app/src/main/java/com/piedpiper/NewRatSightingActivity.java
+++ app/src/main/java/com/piedpiper/NewRatSightingActivity.java
@@ -1,8 +1,12 @@
 package com.piedpiper;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +14,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -28,20 +38,46 @@ public class NewRatSightingActivity extends AppCompatActivity{
     private EditText incidentZip;
     private EditText locationType;
     private TextView createDate;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newratsighting);
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         city = findViewById(R.id.city);
 
         borough = findViewById(R.id.borough);
 
         latitude = findViewById(R.id.latitude);
-
         longitude = findViewById(R.id.longitude);
 
+        int i = 0;
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    i);
+        }
+        if (ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED ) {
+            i = 1;
+        }
+
+        if (i == 1) {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location == null) {
+                                return;
+                            }
+                            latitude.setText(Double.toString(location.getLatitude()));
+                            longitude.setText(Double.toString(location.getLatitude()));
+                        }
+                    });
+        }
         incidentAddress = findViewById(R.id.incidentaddress);
 
         incidentZip = findViewById(R.id.incidentzip);
